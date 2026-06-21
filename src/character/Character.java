01fe7a090dc1a6311beccbcaa14d75f83fad0edb
gpeppas <gpeppas@ajpsoft.gr>
2026-06-21 20:42:49 +0300

    Implement abstract Character class with attributes and methods for attack and health management

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,4 +1,25 @@
 package character;
 
-public class Character {
+public abstract class Character {
+
+    protected String name;
+    protected int hp;
+    protected int mp;
+    protected int attack;
+
+    public Character(String name, int hp, int mp, int attack) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.attack = attack;
+    }
+
+    public void attack(Character target) {
+        target.hp -= attack;
+        System.out.println(name + " attacks " + target.name);
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
 }
